9508b0f8fb07df94947df24c091390bd66ab3e00
winnidebz1 <debpuurwinifred@gmail.com>
2023-02-27 16:29:12 -0800

    Puts2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,39 +1,19 @@
-#include "main.h"
 #include <stdio.h>
-#include <unistd.h>
-
-
-/**
- *rev_string - function that reverses a string.
- *@s: table of caracter
- *Return: Void
- */
+#include <string.h>
 
 void rev_string(char *s)
 {
-	long int i = sizeof(s);
-	char *p = s;
-	/* while (*p != '\0') */
-	/* { */
-	/*printf("%c" ,*p++); */
-	/*  } */
-	printf("lent of p = %ld\n", sizeof(p));
+    int i, len;
+    char *p, temp;
 
-	while (i >= 0)
-	{
-		/* printf("i=%d\n",i); */
-		/* printf("siz-i=%ld\n",sizeof(s) - i); */
-		/* printf ("%c", s[i--]); */
-		*(p + (sizeof(s) - i)) = s[i];
-		/* printf("sizeof = %ld \n", sizeof(s)); */
-		/* printf("%c\n", s[i]); */
-		/* p++; */
-		printf("\np=%c\n", *p);
-	       printf("size of p = %ld\n", sizeof(p));
-		i--;
+    len = strlen(s);
+    p = s;
 
-	}
-	printf("\n");
-	/* sleep(2); */
-	/* *s = *p; */
+    for (i = 0; i < len / 2; i++)
+    {
+        temp = *(p + i);
+        *(p + i) = *(p + len - i - 1);
+        *(p + len - i - 1) = temp;
+    }
 }
+

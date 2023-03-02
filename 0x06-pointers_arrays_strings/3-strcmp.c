716fc8abf70a8f11a1e7976d82c4f038ee365be0
winnidebz1 <debpuurwinifred@gmail.com>
2023-03-02 13:54:10 -0800

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/* #include <stdio.h> */
+
+/**
+ *_strcmp - function that compares two strings.
+ *@s1: pointer caracter
+ *@s2: pointer caracter
+ *Return: integer of pointer type
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = strcmp(s1, s2);
+
+	return (i);
+}
